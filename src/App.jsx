e774a218b7564e20d0999f9f21aa9abd8de8e0b0
Gabriel Gallan <gabriel31.gal@gmail.com>
2025-05-30 00:42:34 -0300

    feat: validate form and autenticate user

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import React, { useState } from 'react'
 import LoginForm from './components/login-form/login_form.jsx'
 import Background from './components/background/background.jsx'
 import Layouts from './components/layouts/layouts.jsx'
 import Loader from './components/loader/loader.jsx'
 
 function App() {
+    const [loading, setLoading] = useState(false)
+
     return (
         <Layouts>
-            <LoginForm/>
+            {loading ? (
+                <Loader />
+            ) : (
+                <LoginForm setLoading={setLoading} />
+            )}
         </Layouts>
     )
 }
--- src/components/login-form/login_form.jsx
+++ src/components/login-form/login_form.jsx
@@ -1,15 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './login-form.css'
+import { z } from 'zod'
 
-function LoginForm() {
-    return (
+//Função que retorna o formulário de login, autenticação e loader
+function LoginForm({setLoading}) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const res = useRef()
+
+// => Função que valida o usuário
+    function autenticateUser(email, password) {
+             return (email === 'gabriel@gmail.com') && (password === 'gabi3110')
+            
+        }
+
+// => Função que envia o formulário
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+            const email_schema = z.string().email().safeParse(email)
+            const pass_schema = z.string().min(8).safeParse(password)
+
+            if (email_schema.success && pass_schema.success) {
+                if (autenticateUser(email, password)) {
+                    setLoading(true)
+                } else {
+                    res.current.innerText = 'Invalid email or pass!'
+                }
+            } else {
+                res.current.innerText = 'Invalid form!'
+            }
+
+        
+
+
+    }
+
+// => Retorno do formulário ou loader
+    return  (
         <div className="login-box">
             <h2>Login to Maestro</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="user-box">
                     <input 
                         type="text"
                         required autoComplete='off'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <label>Email</label>
                 </div>
@@ -17,22 +54,20 @@ function LoginForm() {
                     <input 
                         type="password"
                         required autoComplete='off'
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
                     />
                     <label>Password</label>
                 </div>
                 <div className="buttons">
-                    <a href="#">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
+                    <button type='submit'>
                         Log In
-                    </a>
+                    </button>
                 </div>
                 <div className="change">
                     <p>No account ? <a href="#" className="changeButton">Sign Up</a></p>
                 </div>
-                <div className="logResponse"></div>
+                <div className="logResponse" ref={res}></div>
             </form>
         </div>
     )
